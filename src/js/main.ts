ec4de657a31149aeb69c0a6f5f115081a80523f2
cedrictaro-g <cedric.tarou@gmail.com>
2022-12-11 08:46:06 +0900

    first commit

--- src/js/main.ts
+++ src/js/main.ts
@@ -1,16 +1,2 @@
 import '../scss/main.scss'
-import '../scss/tooltip.scss'
-import '../scss/dropdown.scss'
 
-import { Tooltip } from './tooltip'
-import { Dropdown } from './dropdown'
-
-const tooltip = new Tooltip(document.querySelector('.tooltip')!)
-tooltip.init();
-
-const dropdowns = document.querySelectorAll('.dropdown')!
-
-dropdowns.forEach((dropdown): void => {
-  const instance = new Dropdown(dropdown)
-  instance.init()
-})
